01b879f323d405bc89b60bd789a87e0d2d3ad584
MartinBradbury <>
2023-11-06 20:40:37 +0000

    installed figlet and added intorduction terminal art

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ import maskpass
 #pip install maskpass
 from os import system
 #to clear screen
+from pyfiglet import Figlet
+#pip3 install pyfiglet
 #from words import word_list
 
 
@@ -29,7 +31,6 @@ SHEET = GSPREAD_CLIENT.open('userdata')
 
 
 def welcome():
-    system('clear')
     print("Welcome")
     print("[1] Login")
     print("[2] create account")
@@ -107,4 +108,11 @@ user_selection = dict({
     "3": exit
     })  
 
+
+
+f = Figlet(font='slant')
+print(f.renderText('  Welcome'))
+print(f.renderText('         To'))
+print(f.renderText('  Warcraft'))
+print(f.renderText('mini-games'))
 main()
